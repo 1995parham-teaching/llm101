73352bc2a3e7f25e7152f1948e266c4ad26cdea0
Parham Alvani <parham.alvani@gmail.com>
2024-11-04 09:00:35 +0000

    feat: improve the slidev

---
+++ global-top.vue
@@ -0,0 +1,9 @@
+<template>
+  <a href="https://github.com/1995parham-teaching/llm101" target="_blank">
+    <img
+      style="position: absolute; top: 0; right: 0; border: 0; width: 70px"
+      src="./img/forkme_right_orange.png"
+      alt="Fork me on GitHub"
+    />
+  </a>
+</template>
